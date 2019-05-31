dd69c48a93767b2ca8d227e70b6b707bcc467920
saurabhtatasteel <saurabh_tatasteel@yahoo.com>
2019-06-01 01:39:19 +0530

    Commiting for CI demo

--- DemoWebApp/Controllers/HomeController.cs
+++ DemoWebApp/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace DemoWebApp.Controllers
 
 		public ActionResult Contact()
 		{
-			ViewBag.Message = "Contact page changed from Visual Studio for Continuous Integration demo";
+			ViewBag.Message = "Contact page changed from Visual Studio for Continuous Integration demo - testing";
 
 			return View();
 		}
